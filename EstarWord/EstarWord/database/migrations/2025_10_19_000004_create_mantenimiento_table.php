da9ab4d10934295213e183eece61ec309eb17157
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-10-21 09:20:57 +0200

    Modificación del nombre de las clases de las migraciones
    A la hora de hacer las migraciones me daba error debido al nombre de las clases. Lo he modificado y ahora me funciona.

--- EstarWord/EstarWord/database/migrations/2025_10_19_000004_create_mantenimiento_table.php
+++ EstarWord/EstarWord/database/migrations/2025_10_19_000004_create_mantenimiento_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMantenimientosTable extends Migration
+class CreateMantenimientoTable extends Migration
 {
     public function up()
     {
